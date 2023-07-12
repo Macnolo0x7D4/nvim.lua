f558d12e30bd4e2028918c29eaef674e56b8a0de
Manuel Diaz <yosoymacnolo@gmail.com>
2023-07-12 14:42:09 -0600

    feat: add test suite keybind

--- after/plugin/tests.lua
+++ after/plugin/tests.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>t", vim.cmd.TestNearest)
-vim.keymap.set("n", "<leader>T", vim.cmd.TestFile)
+vim.keymap.set("n", "<leader>tf", vim.cmd.TestFile)
+vim.keymap.set("n", "<leader>T", vim.cmd.TestSuite)
