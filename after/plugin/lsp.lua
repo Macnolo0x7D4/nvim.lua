bcaf1ef2a309ef52867389b80e2d2a84a15a5ef2
Manuel Diaz <yosoymacnolo@gmail.com>
2023-11-02 13:56:58 -0600

    fix: enable github copilot tab key

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,6 +17,9 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
 local lspkind = require('lspkind')
 
 lsp.setup_nvim_cmp({
