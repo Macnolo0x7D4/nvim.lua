25e747e8a4d967c1d9135c8e71120824c733b596
Manuel Diaz <yosoymacnolo@gmail.com>
2024-05-14 17:51:55 -0600

    feat: add hop for easy motion

---
+++ lua/macnolo/lazy/hop.lua
@@ -0,0 +1,17 @@
+return {
+  "phaazon/hop.nvim",
+  branch = 'v2',
+  config = function()
+    local hop = require('hop')
+
+    hop.setup({})
+
+    vim.keymap.set('n', '<leader>hl', function()
+      hop.hint_char1({ current_line_only = true })
+    end)
+
+    vim.keymap.set('n', '<leader>hd', function()
+      hop.hint_char2({ current_line_only = false })
+    end)
+  end
+}
