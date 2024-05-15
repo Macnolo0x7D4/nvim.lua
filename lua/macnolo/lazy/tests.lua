cea7480ea6f4928267e9fd68c026f7f760ea275c
Manuel Diaz <yosoymacnolo@gmail.com>
2024-05-14 19:24:57 -0600

    feat: use neotest

--- lua/macnolo/lazy/tests.lua
+++ lua/macnolo/lazy/tests.lua
@@ -1,8 +1,21 @@
 return {
-  "vim-test/vim-test",
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter"
+  },
   config = function()
-    vim.keymap.set("n", "<leader>t", vim.cmd.TestNearest)
-    vim.keymap.set("n", "<leader>tf", vim.cmd.TestFile)
-    vim.keymap.set("n", "<leader>T", vim.cmd.TestSuite)
+    local neotest = require("neotest")
+
+    vim.keymap.set("n", "<leader>t", function()
+      neotest.run.run()
+    end)
+
+    vim.keymap.set("n", "<leader>tf", function()
+      neotest.run.run(vim.fn.expand("%"))
+    end)
+    --vim.keymap.set("n", "<leader>T", vim.cmd.TestSuite)
   end
 }
