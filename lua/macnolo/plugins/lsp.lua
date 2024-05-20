34ce852e974287809fe37e3ceab22e047e3544c7
Manuel Diaz <yosoymacnolo@gmail.com>
2024-05-20 11:21:03 -0600

    fix: remove tab as lsp keybinding

--- lua/macnolo/plugins/lsp.lua
+++ lua/macnolo/plugins/lsp.lua
@@ -69,7 +69,6 @@ return {
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
       }),
       sources = cmp.config.sources({
