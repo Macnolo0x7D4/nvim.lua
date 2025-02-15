f2d76696c3ae87c530a76a7bfd0b0875dbe3b25c
Manuel Diaz <yosoymacnolo@gmail.com>
2025-02-15 17:32:58 -0600

    refactor: merge lualine with colors

--- lua/macnolo/plugins/colors.lua
+++ lua/macnolo/plugins/colors.lua
@@ -25,17 +25,87 @@ return {
     end
   },
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     config = function()
-      require('rose-pine').setup({
-        disable_background = true,
-        styles = {
-          italic = false,
-        },
+      require("catppuccin").setup({
+        flavour = "mocha"
       })
 
-      colorize("rose-pine")
+      colorize("catppuccin")
     end
   },
+  --{
+  --"rose-pine/neovim",
+  --name = "rose-pine",
+  --config = function()
+  --require('rose-pine').setup({
+  --disable_background = true,
+  --styles = {
+  --italic = false,
+  --},
+  --})
+
+  --colorize("rose-pine")
+  --end
+  --},
+  --{
+  --"shaunsingh/moonlight.nvim",
+  --name = "moonlight",
+  --config = function()
+  --colorize("moonlight")
+  --end
+  --}
+  {
+    "hoob3rt/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          --theme = 'gruvbox',
+          --theme = 'palenight',
+          --theme = 'moonlight',
+          theme = 'catppuccin',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+          }
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {}
+      }
+    end
+  }
 }
