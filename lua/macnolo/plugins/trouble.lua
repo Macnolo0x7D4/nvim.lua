521bdb8b6dd6c0d79b641f7a4acb0496aada2f8e
Manuel Díaz <yosoymacnolo@gmail.com>
2025-06-01 15:06:44 -0600

    feat: install trouble

---
+++ lua/macnolo/plugins/trouble.lua
@@ -0,0 +1,29 @@
+return {
+  "folke/trouble.nvim",
+  opts = {
+    modes = {
+      test = {
+        mode = "diagnostics",
+        preview = {
+          type = "split",
+          relative = "win",
+          position = "right",
+          size = 0.3,
+        },
+      },
+    },
+  },
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+  },
+}
