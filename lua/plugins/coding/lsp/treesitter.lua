f19d6c633ac08582ba5631bef30615470d7ce378
Manuel Díaz <yosoymacnolo@gmail.com>
2025-07-14 22:31:36 -0600

    revert: fix treesitter config

--- lua/plugins/coding/lsp/treesitter.lua
+++ lua/plugins/coding/lsp/treesitter.lua
@@ -2,34 +2,36 @@ return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "c",
-      "cpp",
-      "java",
-      "kotlin",
-      "lua",
-      "vim",
-      "elixir",
-      "erlang",
-      "heex",
-      "html",
-      "css",
-      "javascript",
-      "typescript",
-      "go",
-      "rust",
-      "markdown",
-      "markdown_inline",
-      "yaml"
-    },
-    sync_install = false,
-    indent = {
-      enable = true
-    },
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { "markdown" },
-    },
-  }
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "c",
+        "cpp",
+        "java",
+        "kotlin",
+        "lua",
+        "vim",
+        "elixir",
+        "erlang",
+        "heex",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "go",
+        "rust",
+        "markdown",
+        "markdown_inline",
+        "yaml"
+      },
+      sync_install = false,
+      indent = {
+        enable = true
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { "markdown" },
+      },
+    })
+  end
 }
