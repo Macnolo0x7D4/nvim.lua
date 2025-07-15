f26fd024ab8d16a15cca10338c6a84e6e2d6349b
Manuel Díaz <yosoymacnolo@gmail.com>
2025-07-14 19:23:14 -0600

    refactor: remove old config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,8 +16,3 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 
 vim.opt.clipboard:append("unnamedplus")
-
-if vim.g.neovide then
-  vim.o.guifont = "BerkeleyMono Nerd Font:h12"
-  vim.opt.linespace = 3
-end
