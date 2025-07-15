5bbf10e4f39262256945342d7dbe5e21d94eee87
Manuel Díaz <yosoymacnolo@gmail.com>
2025-07-14 21:05:22 -0600

    feat: reorganize lsp code

--- lua/plugins/coding/lsp/config.lua
+++ lua/plugins/coding/lsp/config.lua
@@ -15,19 +15,17 @@ return {
           prefix = "",
         },
       })
-
-      vim.keymap.set("n", "<leader>F", vim.lsp.buf.format)
       
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = true }),
+      vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = args.buf,
-            callback = function()
-              vim.lsp.buf.format { async = false, client_id = args.data.client_id, bufnr = args.buf }
-            end,
-          })
-        end
+          local opts = { noremap = true, silent = true, buffer = args.buf }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        end,
       })
     end
   }
