147069e297512ebab01f7e02d35ac60015a0343d
Manuel Díaz <yosoymacnolo@gmail.com>
2025-07-14 22:59:53 -0600

    feat: add keys to neotest

--- lua/plugins/coding/tests/tests.lua
+++ lua/plugins/coding/tests/tests.lua
@@ -8,9 +8,13 @@ return {
     "jfpedroza/neotest-elixir"
   },
   keys = {
-    {"<leader>tt", function() require("neotest").run.run() end},
-    {"<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end},
-    {"<leader>T", vim.cmd.TestSuite}
+    { "<leader>tt", function() require("neotest").run.run() end, desc = "Run Nearest (Neotest)" },
+    { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File (Neotest)" },
+    { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files (Neotest)" },
+    { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last (Neotest)" },
+    { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary (Neotest)" },
+    { "<leader>to", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel (Neotest)" },
+    { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop (Neotest)" },
   },
   config = function()
     require("neotest").setup({
