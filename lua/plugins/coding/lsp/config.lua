599b1dee2438d4f35247324144660110ba711ee6
Manuel Díaz <yosoymacnolo@gmail.com>
2025-07-14 22:07:56 -0600

    fix: update lsp keymaps

--- lua/plugins/coding/lsp/config.lua
+++ lua/plugins/coding/lsp/config.lua
@@ -22,9 +22,9 @@ return {
           vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
           vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
           vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "grn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "gca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "grf", vim.lsp.buf.references, opts)
         end,
       })
     end
