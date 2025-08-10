d3be6d7625f6d03ad464efe5f1d40bfbe92f525e
Manuel Díaz <yosoymacnolo@gmail.com>
2025-08-09 19:05:26 -0600

    feat: add flash nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,8 +3,6 @@ vim.g.mapleader = " "
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
 vim.keymap.set("n", "<leader>wv", "<C-w>v")
 vim.keymap.set("n", "<leader>ws", "<C-w>s")
 vim.keymap.set("n", "<leader>wr", "<C-w>=")
--- lua/plugins/editor/motion.lua
+++ lua/plugins/editor/motion.lua
@@ -1,20 +1,15 @@
 return {
   {
-    "phaazon/hop.nvim",
-    branch = 'v2',
-    config = function()
-      local hop = require('hop')
-
-      hop.setup({})
-
-      vim.keymap.set('n', '<leader>hl', function()
-        hop.hint_char1({ current_line_only = true })
-      end)
-
-      vim.keymap.set('n', '<leader>hd', function()
-        hop.hint_char2({ current_line_only = false })
-      end)
-    end
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   },
   {
     "windwp/nvim-autopairs",
