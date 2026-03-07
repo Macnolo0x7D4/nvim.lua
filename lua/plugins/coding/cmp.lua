f24788ede7f830b8502c816cfaf386a282437565
Manuel Díaz <diaz@macnolo.net>
2026-03-07 13:56:02 -0600

    feat: add new style to nvim-cmp box

--- lua/plugins/coding/cmp.lua
+++ lua/plugins/coding/cmp.lua
@@ -14,7 +14,7 @@ return {
     config = function()
       local cmp = require('cmp')
       local cmp_lsp = require("cmp_nvim_lsp")
-      
+
       local capabilities = vim.tbl_deep_extend(
         "force",
         {},
@@ -22,7 +22,7 @@ return {
         cmp_lsp.default_capabilities())
 
       _G.cmp_nvim_lsp_capabilities = capabilities
-      
+
       local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
       cmp.setup({
@@ -31,11 +31,62 @@ return {
             require('luasnip').lsp_expand(args.body)
           end,
         },
+        window = {
+          completion = cmp.config.window.bordered({
+            border = 'rounded',
+            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            col_offset = -3,
+            side_padding = 0,
+          }),
+          documentation = cmp.config.window.bordered({
+            border = 'rounded',
+          }),
+        },
+        formatting = {
+          fields = { 'kind', 'abbr', 'menu' },
+          format = function(entry, vim_item)
+            local kind_icons = {
+              Text = "󰉿",
+              Method = "󰆧",
+              Function = "󰊕",
+              Constructor = "",
+              Field = "󰜢",
+              Variable = "󰀫",
+              Class = "󰠱",
+              Interface = "",
+              Module = "",
+              Property = "󰜢",
+              Unit = "󰑭",
+              Value = "󰎠",
+              Enum = "",
+              Keyword = "󰌋",
+              Snippet = "",
+              Color = "󰏘",
+              File = "󰈙",
+              Reference = "󰈇",
+              Folder = "󰉋",
+              EnumMember = "",
+              Constant = "󰏿",
+              Struct = "󰙅",
+              Event = "",
+              Operator = "󰆕",
+              TypeParameter = "",
+              Copilot = "󰘧",
+            }
+
+            -- Set the icon
+            vim_item.kind = kind_icons[vim_item.kind]
+
+            return vim_item
+          end,
+        },
         mapping = cmp.mapping.preset.insert({
           ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
           ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
           ['<C-y>'] = cmp.mapping.confirm({ select = true }),
           ["<C-Space>"] = cmp.mapping.complete(),
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
